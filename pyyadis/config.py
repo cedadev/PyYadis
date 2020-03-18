bc56036e896b6cca6ddea6fbeaa3802bc392fe3d
Philip Kershaw <Philip.Kershaw@stfc.ac.uk>
2020-03-18 15:56:34 +0000

    Added configuration file setting and fixed response type

---
+++ pyyadis/config.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+"""Yadis Flask app config settings."""
+
+__author__ = """Philip Kershaw"""
+__contact__ = "philip.kershaw@stfc.ac.uk"
+__copyright__ = "Copyright 2020 United Kingdom Research and Innovation"
+__license__ = "BSD - see LICENSE file in top-level package directory"
+# Path prefix for OpenID URIs.  ESGF stock implementation uses
+# '/esgf-idp/openid/'
+OPENID_URI_PATH_PREFIX = '/openid/'
+#OPENID_URI_PATH_PREFIX = '/esgf-idp/openid/'
+
+ATTRIBUTE_SERVICE_URI = 'https://sandstorm.ceda.ac.uk/attribute-service/'
+MYPROXY_SERVICE_URI = 'socket://slcs1.ceda.ac.uk:7512'
+SLCS_URI = 'https://slcs.ceda.ac.uk/onlineca/certificate/'
+OAUTH_ACCESS_TOK_URI = 'https://slcs.ceda.ac.uk/oauth/access_token'
+OAUTH_RESOURCE_URI = 'https://slcs.ceda.ac.uk/oauth/certificate/'
+OAUTH_AUTHORIZE_URI = 'https://slcs.ceda.ac.uk/oauth/authorize'
--- pyyadis/flask_app.py
+++ pyyadis/flask_app.py
@@ -1,95 +1,101 @@
 # -*- coding: utf-8 -*-
 
-"""Main module."""
+"""Flask callbacks for returning Yadis responses."""
 
 __author__ = """Philip Kershaw"""
 __contact__ = "philip.kershaw@stfc.ac.uk"
 __copyright__ = "Copyright 2020 United Kingdom Research and Innovation"
 __license__ = "BSD - see LICENSE file in top-level package directory"
-from flask import Flask, escape, request
+from flask import Flask, escape, request, Response
 from markupsafe import escape
+
+
+class YadisFlaskAppConfigError(Exception):
+    "Error with configuration settings for Yadis Flask application"
+
 
 app = Flask(__name__)
 
+# Set configuration from a separate module 
+if not app.config.from_envvar('YADIS_FLASK_APP_CFG_FILEPATH'):
+    raise YadisFlaskAppConfigError('Error loading config file path '
+                                   'Please check the "YADIS_FLASK_APP_CFG_FILEPATH"'
+                                   'environment variable is set to valid file')
 
-ATTRIBUTE_SERVICE_URI = 'https://sandstorm.ceda.ac.uk/attribute-service/'
-MYPROXY_SERVICE_URI = 'socket://slcs1.ceda.ac.uk:7512'
-SLCS_URI = 'https://slcs.ceda.ac.uk/onlineca/certificate/'
-OAUTH_ACCESS_TOK_URI = 'https://slcs.ceda.ac.uk/oauth/access_token'
-OAUTH_RESOURCE_URI = 'https://slcs.ceda.ac.uk/oauth/certificate/'
-OAUTH_AUTHORIZE_URI = 'https://slcs.ceda.ac.uk/oauth/authorize'
 
-@app.route('/openid/<username>')
+@app.route(f'{app.config["OPENID_URI_PATH_PREFIX"]}/<username>')
 def yadis_personal_response(username):
     '''Respond to Yadis calls with personal identifier contained in the OpenID'''
-    return f'''<?xml version="1.0" encoding="UTF-8"?>
+    return Response(f'''<?xml version="1.0" encoding="UTF-8"?>
 <xrds:XRDS xmlns:xrds="xri://$xrds" xmlns="xri://$xrd*($v*2.0)">
     <XRD>
         <Service priority="20">
             <Type>urn:esg:security:attribute-service</Type>
-            <URI>{ATTRIBUTE_SERVICE_URI}</URI>
+            <URI>{app.config["ATTRIBUTE_SERVICE_URI"]}</URI>
             <LocalID>{request.url}</LocalID>
         </Service>
         <Service priority="10">
             <Type>urn:esg:security:myproxy-service</Type>
-            <URI>{MYPROXY_SERVICE_URI}</URI>
+            <URI>{app.config["MYPROXY_SERVICE_URI"]}</URI>
             <LocalID>{request.url}</LocalID>
         </Service>
         <Service priority="10">
             <Type>urn:esg:security:slcs</Type>
-            <URI>{SLCS_URI}</URI>
+            <URI>{app.config["SLCS_URI"]}</URI>
             <LocalID>{request.url}</LocalID>
         </Service>
         <Service priority="5">
             <Type>urn:esg:security:oauth:endpoint:access</Type>
-            <URI>{OAUTH_ACCESS_TOK_URI}</URI>
+            <URI>{app.config["OAUTH_ACCESS_TOK_URI"]}</URI>
             <LocalID>{request.url}</LocalID>
         </Service>
         <Service priority="5">
             <Type>urn:esg:security:oauth:endpoint:resource</Type>
-            <URI>{OAUTH_RESOURCE_URI}</URI>
+            <URI>{app.config["OAUTH_RESOURCE_URI"]}</URI>
             <LocalID>{request.url}</LocalID>
         </Service>
         <Service priority="5">
             <Type>urn:esg:security:oauth:endpoint:authorize</Type>
-            <URI>{OAUTH_AUTHORIZE_URI}</URI>
+            <URI>{app.config["OAUTH_AUTHORIZE_URI"]}</URI>
             <LocalID>{request.url}</LocalID>
         </Service>
    </XRD>
 </xrds:XRDS>
-'''
+''', mimetype='text/xml')
 
 @app.route('/openid/')
 def yadis_general_response():
     """Generic response for Yadis call where the OpenID is generalised for the IdP and
     contains no personal identifier"""
-    return f'''<?xml version="1.0" encoding="UTF-8"?>
+    return Response(f'''<?xml version="1.0" encoding="UTF-8"?>
 <xrds:XRDS xmlns:xrds="xri://$xrds" xmlns="xri://$xrd*($v*2.0)">
     <XRD>
         <Service priority="20">
             <Type>urn:esg:security:attribute-service</Type>
-            <URI>{ATTRIBUTE_SERVICE_URI}</URI>
+            <URI>{app.config["ATTRIBUTE_SERVICE_URI"]}</URI>
         </Service>
         <Service priority="10">
             <Type>urn:esg:security:myproxy-service</Type>
-            <URI>{MYPROXY_SERVICE_URI}</URI>
+            <URI>{app.config["MYPROXY_SERVICE_URI"]}</URI>
         </Service>
         <Service priority="10">
             <Type>urn:esg:security:slcs</Type>
-            <URI>{SLCS_URI}</URI>
+            <URI>{app.config["SLCS_URI"]}</URI>
         </Service>
         <Service priority="5">
             <Type>urn:esg:security:oauth:endpoint:access</Type>
-            <URI>{OAUTH_ACCESS_TOK_URI}</URI>
+            <URI>{app.config["OAUTH_ACCESS_TOK_URI"]}</URI>
         </Service>
         <Service priority="5">
             <Type>urn:esg:security:oauth:endpoint:resource</Type>
-            <URI>{OAUTH_RESOURCE_URI}</URI>
+            <URI>{app.config["OAUTH_RESOURCE_URI"]}</URI>
         </Service>
         <Service priority="5">
             <Type>urn:esg:security:oauth:endpoint:authorize</Type>
-            <URI>{OAUTH_AUTHORIZE_URI}</URI>
+            <URI>{app.config["OAUTH_AUTHORIZE_URI"]}</URI>
         </Service>
    </XRD>
 </xrds:XRDS>
-'''
+''', mimetype='text/xml')
+
+
